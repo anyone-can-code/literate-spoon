6c5f3ff4a9664ec9235c6aaa59224ad38a98f28f
Comrade SparklePony <comradesparklepony@gmail.com>
2018-11-30 15:50:46 -0500

    Finish getting rid of rooms.txt

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	public static Engine game;
 
 	public static void main(String args[]) {
-		game = new Engine("src/engine/rooms.txt");
+		game = new Engine();
 		
 		game.addWord(new Verb("move go walk run climb jog travel journey venture", (Word w, Engine t) -> {
 			if (w.getClass() != Direction.class) {
