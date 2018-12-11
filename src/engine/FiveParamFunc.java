ae99344251fd2fe11ca0b9bf8dca3f24bc233e72
maocin <44908364+maocin@users.noreply.github.com>
2018-12-11 09:56:35 -0500

    we all knew where this trend was going

---
+++ src/engine/FiveParamFunc.java
@@ -0,0 +1,5 @@
+package engine;
+
+public interface FiveParamFunc<O1, O2, O3, O4, O5> {
+	public void accept(O1 o1, O2 o2, O3 o3, O4 o4, O5 o5);
+}
---
+++ src/engine/FourParamFunc.java
@@ -0,0 +1,5 @@
+package engine;
+
+public interface FourParamFunc<O1, O2, O3, O4> {
+	public void accept(O1 o1, O2 o2, O3 o3, O4 o4);
+}
